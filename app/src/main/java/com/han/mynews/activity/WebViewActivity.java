c6d86a58dcc37c15b7fd1fbbb0c4c8f3aa524e4e
han <>
2018-08-18 21:00:01 +0900

    no message

--- app/src/main/java/com/han/mynews/activity/WebViewActivity.java
+++ app/src/main/java/com/han/mynews/activity/WebViewActivity.java
@@ -1,30 +1,13 @@
-package com.han.mynews;
+package com.han.mynews.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.han.mynews.dto.Item;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.han.mynews.R;
 
 public class WebViewActivity extends AppCompatActivity {
 
